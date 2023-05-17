ff4af51f03f7e021afecfcad0b8d8c741e66520a
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-05-17 22:49:43 +0600

    add readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+## Installation
+
+- React Router Dom v6.4 : Documentation Link -> [Link](https://reactrouter.com/en/main/start/overview)
---
+++ src/Page/Home/HeroSection/HeroSection.jsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+  return (
+    <div>
+      <h1>hero section</h1>
+    </div>
+  );
+};
+
+export default HeroSection;
