c0a4fda48a15643b73f37e75bfdcb276d8ced06c
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-05-20 14:40:50 +0600

    added extra section why choose us

--- src/Page/Home/WhyChose/WhyChose.jsx
+++ src/Page/Home/WhyChose/WhyChose.jsx
@@ -1,7 +1,45 @@
+import log1 from "../../../assets/images/icon-1.png";
+import log2 from "../../../assets/images/icon-2.png";
+import log3 from "../../../assets/images/icon-3.png";
+
 const WhyChose = () => {
   return (
-    <div>
-      <h1>ehay</h1>
+    <div className="bg-[#363636] text-white ">
+      <h1 className="text-center text-4xl lg:text-6xl font-extrabold pt-16 pb-10 ">
+        WHY CHOOSE US
+      </h1>
+      <div className="my-cunstom-container grid grid-cols-1 md:grid-cols-3 pb-24 ">
+        <div className="text-center lg:text-left my-10 lg:my-4 lg:pb-24">
+          <img className="h-14 w-12 mx-auto lg:mx-0 mb-3" src={log1} alt="" />
+          <h1 className="text-1xl lg:text-3xl font-bold py-4">
+            SAFETY & SECURITY
+          </h1>
+          <p className="lg:w-4/5 ">
+            variations of passages of Lorem Ipsum available, but the majority
+            have
+          </p>
+        </div>
+        <div className="text-center lg:text-left my-10 lg:my-4">
+          <img className="h-14 w-12 mx-auto lg:mx-0 mb-3" src={log2} alt="" />
+
+          <h1 className="text-1xl lg:text-3xl font-bold py-4">
+            Online Booking
+          </h1>
+          <p className="lg:w-4/5 ">
+            variations of passages of Lorem Ipsum available, but the majority
+            have
+          </p>
+        </div>
+        <div className="text-center lg:text-left my-10 lg:my-4">
+          <img className="h-14 w-12 mx-auto lg:mx-0 mb-3" src={log3} alt="" />
+
+          <h1 className="text-1xl lg:text-3xl font-bold py-4">Best Drivers</h1>
+          <p className="lg:w-4/5 ">
+            variations of passages of Lorem Ipsum available, but the majority
+            have
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Page/Sharied/Footer/Footer.jsx
+++ src/Page/Sharied/Footer/Footer.jsx
@@ -3,28 +3,28 @@ import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer grid grid-cols-2 lg:grid-cols-4 p-10 bg-black text-white ">
-        <div>
-          <span className="footer-title mx-auto">Services</span>
+      <footer className="footer  grid grid-cols-2 lg:grid-cols-4 py-10  pl-10 bg-black text-white ">
+        <div className="pl-16">
+          <span className="footer-title  ">Services</span>
           <a className="link  link-hover">Branding</a>
           <a className="link  link-hover">Design</a>
           <a className="link  link-hover">Marketing</a>
           <a className="link  link-hover">Advertisement</a>
         </div>
-        <div>
+        <div className="pl-16">
           <span className="footer-title">Company</span>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Jobs</a>
           <a className="link link-hover">Press kit</a>
         </div>
-        <div>
+        <div className="pl-16">
           <span className="footer-title">Legal</span>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </div>
-        <div>
+        <div className="pl-16">
           <span className="footer-title">Legal</span>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
